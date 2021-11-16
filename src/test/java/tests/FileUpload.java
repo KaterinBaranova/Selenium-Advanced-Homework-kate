0a78e609572d74cdbfe600acd66927889840fbf1
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2021-11-16 21:22:24 +0300

    file uplod

--- src/test/java/tests/FileUpload.java
+++ src/test/java/tests/FileUpload.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.time.Duration;
 
 public class FileUpload extends BaseTest {
@@ -16,6 +17,8 @@ public class FileUpload extends BaseTest {
         driver.get("http://the-internet.herokuapp.com/upload");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement chooseFileButton = driver.findElement(By.id("file-upload"));
+        /*File file = new File ("/Users/ekaterinabaranova/Documents/Homework Kate_Selenium Advanced/src/main/1500478632132917686.jpeg")
+        File.getAbsolutePath();*/
         chooseFileButton.sendKeys("/Users/ekaterinabaranova/Documents/Homework Kate_Selenium Advanced/src/main/1500478632132917686.jpeg");
         WebElement uploadButton = driver.findElement(By.id("file-submit"));
         uploadButton.click();
