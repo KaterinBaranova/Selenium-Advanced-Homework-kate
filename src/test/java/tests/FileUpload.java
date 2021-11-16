23f6c5582c6131b13fceb9e32dfd6be9da927cf1
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2021-11-16 20:00:48 +0300

    upload file fix

--- src/test/java/tests/FileUpload.java
+++ src/test/java/tests/FileUpload.java
@@ -2,7 +2,7 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -20,7 +20,7 @@ public class FileUpload extends BaseTest {
         WebElement uploadButton = driver.findElement(By.id("file-submit"));
         uploadButton.click();
         WebElement file = driver.findElement(By.id("uploaded-files"));
-        wait.until(ExpectedCondition.presenceOfElementLocated(By.id("uploaded-files")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("uploaded-files")));
         Assert.assertEquals(file.getText(), "1500478632132917686.jpeg");
     }
 }
