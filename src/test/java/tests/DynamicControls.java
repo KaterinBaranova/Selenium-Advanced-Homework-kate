ea6dd5a1eb445b43369b3d36ba6031ed4a789e29
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2021-11-16 17:52:18 +0300

    Homework selenium Advanced

---
+++ src/test/java/tests/DynamicControls.java
@@ -0,0 +1,45 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DynamicControls extends BaseTest {
+
+
+    @Test
+    public void enableDisableAndCheckboxTest() {
+        driver.get("http://the-internet.herokuapp.com/dynamic_controls");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement checkbox = driver.findElement(By.cssSelector("[type=checkbox]"));
+        checkbox.click();
+        WebElement removeButton = driver.findElement(By.xpath("//*[@id='checkbox-example']/descendant::button[@type='button']"));
+        removeButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        wait.until(ExpectedConditions.invisibilityOf(checkbox));
+        removeButton.click(); // remove button becomes add button
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("checkbox")));
+        // Enable/disable
+        WebElement enableDisableButton = driver.findElement(By.xpath("//*[@id='input-example']/descendant::button[@type='button']"));
+        Assert.assertEquals(enableDisableButton.getText(), "Enable");
+        enableDisableButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        WebElement message = driver.findElement(By.id("message"));
+        Assert.assertEquals(message.getText(), "It's enabled!");
+        Assert.assertEquals(enableDisableButton.getText(), "Disable");
+        enableDisableButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
+        message = driver.findElement(By.id("message"));
+        Assert.assertEquals(message.getText(), "It's disabled!");
+        enableDisableButton.click();
+        Assert.assertEquals(enableDisableButton.getText(), "Enable");
+    }
+}
+
+
