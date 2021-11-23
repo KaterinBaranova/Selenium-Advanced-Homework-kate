3b845a81cdaf2b56dfdb0294dd3a129b8eda6281
Kate Shauliuk <Katsiarynabaranava@gmail.com>
2021-11-23 12:12:08 +0300

    fixes in filedownload tasks

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,25 +3,38 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
     protected Actions actions;
+    protected WebDriverWait wait;
+    protected  String testDownloadsDir;
 
+    @BeforeClass(alwaysRun = true)
+    public void setUp() {
+        testDownloadsDir = System.getProperty("user.dir") + File.separator + "target/downloads";
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", testDownloadsDir);
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
 
-    @BeforeClass (alwaysRun = true)
-    public void setup() {
         WebDriverManager.chromedriver().setup();
-        this.driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
         actions = new Actions(driver);
-        System.out.println(driver == null);
+        wait = new WebDriverWait(driver, 10);
     }
 
     @AfterClass(alwaysRun = true)
